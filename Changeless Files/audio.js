a73ce7dfc4b07ca1e5fdaf420a846ddea00b4db5
Micheal Cole Young <coleyoungmusic@gmail.com>
2018-02-25 00:42:26 -0600

    Create audio.js

---
+++ Changeless Files/audio.js
@@ -0,0 +1,9 @@
+function playAudio(id) {
+  play = document.getElementById(id).play();
+  return play;
+}
+
+function pauseAudio(id) {
+  pause = document.getElementById(id).pause();
+  return pause;
+}
